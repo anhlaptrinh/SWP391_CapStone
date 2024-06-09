c48c622dd45ccb6f95ecd561110480cfd2e51769
Lê Quang Ánh <superanhziggy@gmail.com>
2024-06-09 15:24:34 +0700

    api

--- types/jwelry.ts
+++ types/jwelry.ts
@@ -5,3 +5,40 @@ export interface Jwellery {
     productImage:  string;
     id:            string;
 }
+export interface Pagination {
+    items:           Product[];
+    currentPage:     number;
+    pageSize:        number;
+    totalRecords:    number;
+    totalPages:      number;
+    hasNextPage:     boolean;
+    hasPreviousPage: boolean;
+}
+
+export interface Product {
+    productId:        number;
+    productName:      string;
+    percentPriceRate: number;
+    productionCost:   number;
+    status:           boolean;
+    featuredImage:    string;
+    category:         string;
+    productType:      string;
+    gender:           string;
+    colour:           string;
+    gems:             Gem[];
+    materials:        Material[];
+}
+
+export interface Gem {
+    gemName:     string;
+    origin:      string;
+    caratWeight: number;
+    colour:      string;
+    clarity:     string;
+    cut:         string;
+}
+
+export interface Material {
+    materialName: string;
+}

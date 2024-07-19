f147e67a726398783b8149c004398d2c94c5e93f
Lê Quang Ánh <superanhziggy@gmail.com>
2024-07-19 16:05:47 +0700

    payment19072024

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { App as AntdApp } from "antd";
 
 import Router from "@/router/index";
 import AntdConfig from "@/theme/antd";
+import { Route } from "react-router-dom";
+import PaymentSuccess from "./pages/payment/paymentsuccess";
 
 function App() {
   return (
     <AntdConfig>
       <AntdApp>
         <Router />
+        {/* <Route path="/payment/success" element={<PaymentSuccess />} /> */}
       </AntdApp>
     </AntdConfig>
   );
